547a5176db88ec3226945a5a29f4206de62804ea
BryanMM <bryanmasis87@gmail.com>
2020-10-16 17:14:45 -0600

    Added acos_t

---
+++ Parte 1/acos_t.m
@@ -0,0 +1,7 @@
+function xAprox = acos_t(a)
+    if(-1<=a $$ a<=1 && isinteger(a)!=1)
+        xAprox = pi*div_t(2) - asin_t(a);
+    else
+        disp("El número ingresado no está en el rango -1 a 1");
+    endif
+endfunction
