2785bef99aed0bca5747f7c4c779ead2d1f82a12
Alejandra Castrillo <mcastrillo@estudiantec.cr>
2020-10-18 00:37:06 -0600

    Equation 12 from issue #21 is done

--- Parte 2/metodos_p2.py
+++ Parte 2/metodos_p2.py
@@ -102,3 +102,100 @@ def newton_H_m2(fun: str, x0: float, tol: float, iterMax: int):
     error = abs(f(x))
 
     return (x, error)
+
+def newton_G_m1(fun: str, x0: float, tol: float, iterMax: int):
+    """Generalización del método aproximación Newton Raphson por
+    medio del método Halley
+
+    Utiliza una variante del método Newton Raphson con la función 
+    de peso Gw:
+
+        x = x + Gw(x)*(f(x)/df(x))
+
+    Donde: 
+
+        w  = f(x)*f''(x) / (f'(x))**2
+        Gw = 2/2-w
+
+    Devuelve en una tupla el valor de el error y el valor de x
+    donde:
+
+        f(x) = 0
+
+    Parámetros:
+    fun     -- string que representa la función f para la aproximación 
+    x0      -- valor inicial 
+    tol     -- tolerancia debe ser mayor a 0
+    iterMax -- número máximo de iteraciones 
+    
+    """
+    xk          = symbols("xk")
+    
+    f           = parse_expr(fun) 
+    df          = diff(f)
+    w           = (f*diff(df))/(df**2)
+    Gw          = 2/(2-w)
+
+    f           = lambdify(xk, f)
+    df          = lambdify(xk, df)
+    w           = lambdify(xk, w)
+    Gw          = lambdify(xk, Gw)
+    
+    A           = 0.1
+    x           = x0
+    k           = 0
+
+    while (k < iterMax) & (abs(f(x)) > tol):
+         x = x - Gw(x) *f(x) / df(x)
+         k+=1
+    error = abs(f(x))
+
+    return (x, error)
+
+def newton_G_m2(fun: str, x0: float, tol: float, iterMax: int):
+    """Generalización del método aproximación Newton Raphson por
+    medio del método Fang et al. 
+
+    Utiliza una variante del método Newton Raphson con la función 
+    de peso Gw:
+
+        x = x + Gw(x)*(f(x)/df(x))
+
+    Donde: 
+        w  = f(x)*f''(x) / (f'(x))**2
+        Gw = 2/(1+sqrt(1-w))
+
+    Devuelve en una tupla el valor de el error y el valor de x
+    donde:
+
+        f(x) = 0
+
+    Parámetros:
+    fun     -- string que representa la función f para la aproximación 
+    x0      -- valor inicial 
+    tol     -- tolerancia debe ser mayor a 0
+    iterMax -- número máximo de iteraciones 
+    
+    """
+    xk          = symbols("xk")
+    
+    f           = parse_expr(fun) 
+    df          = diff(f)
+    w           = (f*diff(df))/(df**2)
+    Gw          = 2/(1+sqrt(1-w))
+
+    f           = lambdify(xk, f)
+    df          = lambdify(xk, df)
+    w           = lambdify(xk, w)
+    Gw          = lambdify(xk, Gw)
+    
+    A           = 0.1
+    x           = x0
+    k           = 0
+
+    while (k < iterMax) & (abs(f(x)) > tol):
+         x = x - Gw(x) *f(x) / df(x)
+         k+=1
+    error = abs(f(x))
+
+    return (x, error)
