8e2f116cbd02562f1af860af2c854f2c52aa3021
BryanMM <bryanmasis87@gmail.com>
2020-10-18 14:01:00 -0600

    Updated acos_t
    Changed the pi for the function pi_t

--- Parte 1/acos_t.m
+++ Parte 1/acos_t.m
@@ -1,7 +1,7 @@
 function xAprox = acos_t(a)
     if(-1<=a && a<=1 && isinteger(a)!=1)
-        xAprox = pi*div_t(2) - asin_t(a);
+        xAprox = pi_t*div_t(2) - asin_t(a);
     else
-        disp("El número ingresado no está en el rango -1 a 1");
+        disp("El número ingresado no está en el rango");
     endif
 endfunction
